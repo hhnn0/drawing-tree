f09b46dd7c9b73577690646cb265176d420b8df1
hhnn0 <hyun0520ho@gmail.com>
2022-12-22 03:42:35 +0900

    chore: npm install styled components

---
+++ src/components/BottomBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components'
+
+export default function BottomBar() {
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  height: 50px;
+  width: 100%;
+`
